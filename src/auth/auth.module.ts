e93f77e80bd46cc79b8a7e2ef3e8b1955960f177
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2023-06-04 18:57:01 +0500

    перед рефакториногм

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,13 +5,17 @@ import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: 'JWT_SECRET', //TODO создай переменную окружения
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
     }),
   ],
